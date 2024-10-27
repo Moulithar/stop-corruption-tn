5f0954340e18f2f49e36d2bf5c8762c4e98ccf00
Mouli <128670409+Moulithar@users.noreply.github.com>
2024-10-27 22:17:01 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello world 1</h1>
+        <h1>Hello world21</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
